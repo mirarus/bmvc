41dd067401f885e64c5153575e4f0c164e5c9cd4
Ali Güçlü <aliguclutr@gmail.com>
2021-04-16 05:28:53 +0300

    Update Kernel.php

--- System/Kernel.php
+++ System/Kernel.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 1.0
+ * @version 1.1
  */
 
 use Whoops\Run as WhoopsRun;
@@ -44,8 +44,8 @@ final class Kernel
 		if (session_status() !== PHP_SESSION_ACTIVE || session_id() === null) {
 			@ini_set('session.cookie_httponly', 1);
 			@ini_set('session.use_only_cookies', 1);
-			@ini_set('session.gc_maxlifetime', 3600);
-			@session_set_cookie_params(3600);
+			@ini_set('session.gc_maxlifetime', 3600 * 24);
+			@session_set_cookie_params(3600 * 24);
 			
 			session_name("BMVC-MMVC");
 			session_start();
