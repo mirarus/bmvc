cbe2b406c82508153385d20b975f0166440c71dd
Ali Güçlü <aliguclutr@gmail.com>
2021-04-23 01:12:50 +0300

    Update init.php

--- init.php
+++ init.php
@@ -8,10 +8,13 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 1.1
+ * @version 1.2
  */
 
 require_once 'vendor/autoload.php';
 require_once 'App/config.php';
 
-BMVC\Core\App::Run($_config);
+BMVC\Core\App::namespace([
+	'controller' => 'App\Controller\\',
+	'model' => 'App\Model\\'
+])->Run($_config);
