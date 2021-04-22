72234e08e72d81785f2d11b9972a610fb75bea83
Ali Güçlü <aliguclutr@gmail.com>
2021-04-22 06:23:57 +0300

    Update Lib.php

--- System/Libraries/Lib.php
+++ System/Libraries/Lib.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 1.0
+ * @version 1.1
  */
 
 namespace BMVC\Libs;
@@ -18,13 +18,13 @@ class Lib
 
 	public function __call($class, $method)
 	{
-		$class = __NAMESPACE__ . DIRECTORY_SEPARATOR . $class;
+		$class = __NAMESPACE__ . '\\' . $class;
 		return new $class;
 	}
 
 	public static function __callStatic($class, $method)
 	{
-		$class = __NAMESPACE__ . DIRECTORY_SEPARATOR . $class;
+		$class = __NAMESPACE__ . '\\' . $class;
 		return new $class;
 	}
 }
